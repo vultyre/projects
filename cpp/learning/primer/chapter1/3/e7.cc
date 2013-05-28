0a90f1f42b7bafdc2631934148d03378d8cf785b
Vultyre <vultyrus@gmail.com>
2013-05-28 12:10:29 -0500

    some more stuff

---
+++ cpp/learning/primer/chapter1/3/e7.cc
@@ -0,0 +1,13 @@
+//Compile a program that has incorrectly nested comments
+
+/*
+ * Comment /* */ Oh sheet.
+ * jfdkjfdkfkdsfkds
+ * fkdjkfdjkfjdskjfk
+ */
+
+int main()
+{
+	return 0;
+}
+
---
+++ cpp/learning/primer/chapter1/4/e11.cc
@@ -0,0 +1,18 @@
+//Prompt for two integers and print all numbers in the range inclusive
+
+#include <iostream>
+
+int main()
+{
+	int v1 = 0, v2 = 0;
+
+	//Prompt
+	std::cout << "Please enter two integers, smallest first: " << std::endl;
+	std::cin >> v1 >> v2;
+
+	//Loop
+	while (v1 <= v2) {
+		std::cout << v1 << std::endl;
+		v1++;
+	}
+}
