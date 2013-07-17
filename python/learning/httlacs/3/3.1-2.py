51ae9408f406af89c91adfcbf4d1de963a822190
Timothy Rooney <vultyre@voyager.(none)>
2013-07-17 09:41:51 -0500

    some more work in the morning

---
+++ python/learning/httlacs/3/3.1-2.py
@@ -0,0 +1,14 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("Hello!")
+
+tess = turtle.Turtle()
+tess.color("blue")
+tess.pensize(3)
+
+tess.forward(50)
+tess.left(120)
+tess.forward(50)
+
+wn.mainloop()
